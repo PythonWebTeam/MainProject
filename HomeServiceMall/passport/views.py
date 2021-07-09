ac84ef11e6967149c97bf2e438eb468bb8731708
hofee <2716863922@qq.com>
2021-07-09 19:13:41 +0800

    upd

--- HomeServiceMall/passport/views.py
+++ HomeServiceMall/passport/views.py
@@ -4,13 +4,15 @@ from django.views import View
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
+
 
 # 登录页面视图
 class LoginView(View):
     def get(self, request):
         if request.session.get("username"):
-            return HttpResponse("用户 "+request.session.get("username")+",您已登录")
+            return HttpResponse("用户 " + request.session.get("username") + ",您已登录")
         return render(request, "login.html")
 
     def post(self, request):
@@ -40,19 +42,20 @@ class RegisterView(View):
     def post(self, request):
         # TODO:将注册post数据写入数据库
         data = request.POST
+
         username = data.get("user-name")
         password = data.get("user-password")
         phone_number = data.get("phone-number")
         email = data.get("user-email")
         addr = data.get("address")
         prov = data.get("prov")
-        city = data.get("city")
         county = data.get("county")
         msg = "注册成功"
-        if not User.objects.filter(username=username):
-            return render("login.html", {"msg": "用户名已被注册!"})
-        elif not username:
+
+        if not username:
             return render("login.html", {"msg": "用户名不能为空!"})
+        elif not User.objects.filter(username=username):
+            return render("login.html", {"msg": "用户名已被注册!"})
         elif not password:
             return render("login.html", {"msg": "电话号码不能为空!"})
         elif not email:
