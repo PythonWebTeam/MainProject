47e1688c34a8962e203510d6c780959543268f72
hofee <2716863922@qq.com>
2021-07-12 09:46:37 +0800

    update

--- HomeServiceMall/passport/views.py
+++ HomeServiceMall/passport/views.py
@@ -53,7 +53,6 @@ class RegisterView(View):
             return HttpResponse("该手机号已被注册")
         code_rec = data.get("email_code")
         code_db = EmailVerifyRecord.objects.filter(email=email)[0].code
-        print(code_rec,code_db)
         if code_rec != code_db:
             return HttpResponse("邮箱验证码错误")
         user = User.objects.create_user(username=username, password=password, phone=phone_number, email=email,
