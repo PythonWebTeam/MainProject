4706ac813af058ba807ecc64b47d24848abb1676
hofee <2716863922@qq.com>
2021-07-14 11:54:44 +0800

    update

--- HomeServiceMall/passport/views.py
+++ HomeServiceMall/passport/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import View
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
-from account.models import Service, EmailVerifyRecord
+from account.models import EmailVerifyRecord
 from utils.util import Util
 
 User = get_user_model()
@@ -44,7 +44,7 @@ class RegisterView(View):
         password = data.get("user-password")
         phone_number = data.get("phone-number")
         email = data.get("user-email")
-        addr = data.get("address")
+        address = data.get("address")
         prov = data.get("prov")
         city = data.get("city")
         county = data.get("county")
@@ -59,7 +59,7 @@ class RegisterView(View):
         if code_rec != code_db:
             return HttpResponse("邮箱验证码错误")
         user = User.objects.create_user(username=username, password=password, phone=phone_number, email=email,
-                                        province=prov, district=county, details=addr, city=city)
+                                        province=prov, district=county, details=address, city=city)
         if user:
             return HttpResponse("ok")
 
--- HomeServiceMall/shop/views.py
+++ HomeServiceMall/shop/views.py
@@ -14,40 +14,6 @@ class ServiceView(View):
         service = Service.objects.filter(id=int(se_id))[0]
         return render(request, "service.html",
                       {"service": service, "username": username, "services_sort": services_sort, "is_login": is_login})
-
-
-class ShopView(View):
-    def get(self, request):
-        username, services_sort, is_login = Util.get_basic_info(request)
-        return render(request, "shop.html",
-                      {"username": username, "services_sort": services_sort, "is_login": is_login})
-
-
-class PayView(View):
-    def get(self, request):
-        if not request.session.get("is_login"):
-            return redirect("/passport/login/")
-        username, services_sort, is_login = Util.get_basic_info(request)
-        se_id = request.GET.get("se_id")
-        from_cart = request.GET.get("from_cart")
-        username = request.session.get("username")
-        user = User.objects.filter(username=username)[0]
-        if int(from_cart) == 0:
-            services = Service.objects.filter(id=int(se_id))
-        else:
-            carts = Cart.objects.filter(user_id=user.id)
-            services = []
-            for cart in carts:
-                services.append(cart.service)
-        total_cost = 0
-        for service in services:
-            total_cost += service.price
-        return render(request, "pay.html",
-                      {"services": services, "total_cost": total_cost, "user": user, "username": username,
-                       "services_sort": services_sort, "is_login": is_login})
-
-
-class CartAddView(View):
     def post(self,request):
         if not request.session.get("is_login"):
             return redirect("/passport/login/")
@@ -83,8 +49,35 @@ class CartAddView(View):
                 cart.save()
                 return HttpResponse("购物车该服务时间修改成功")
 
+class ShopView(View):
+    def get(self, request):
+        username, services_sort, is_login = Util.get_basic_info(request)
+        return render(request, "shop.html",
+                      {"username": username, "services_sort": services_sort, "is_login": is_login})
 
 
+class PayView(View):
+    def get(self, request):
+        if not request.session.get("is_login"):
+            return redirect("/passport/login/")
+        username, services_sort, is_login = Util.get_basic_info(request)
+        se_id = request.GET.get("se_id")
+        from_cart = request.GET.get("from_cart")
+        username = request.session.get("username")
+        user = User.objects.filter(username=username)[0]
+        if int(from_cart) == 0:
+            services = Service.objects.filter(id=int(se_id))
+        else:
+            carts = Cart.objects.filter(user_id=user.id)
+            services = []
+            for cart in carts:
+                services.append(cart.service)
+        total_cost = 0
+        for service in services:
+            total_cost += service.price
+        return render(request, "pay.html",
+                      {"services": services, "total_cost": total_cost, "user": user, "username": username,
+                       "services_sort": services_sort, "is_login": is_login})
 
 
 
