c7a59322c4ad4245b3f15f1241343129e54635ad
hofee <2716863922@qq.com>
2021-07-14 13:58:36 +0800

    update

--- HomeServiceMall/shop/views.py
+++ HomeServiceMall/shop/views.py
@@ -3,7 +3,7 @@ import datetime
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import View
-from account.models import Service, User, Cart
+from account.models import Service, User, Cart, Shop
 from utils.util import Util
 
 
@@ -14,7 +14,8 @@ class ServiceView(View):
         service = Service.objects.filter(id=int(se_id))[0]
         return render(request, "service.html",
                       {"service": service, "username": username, "services_sort": services_sort, "is_login": is_login})
-    def post(self,request):
+
+    def post(self, request):
         if not request.session.get("is_login"):
             return redirect("/passport/login/")
         data = request.POST
@@ -29,8 +30,8 @@ class ServiceView(View):
         if CartNum >= 10:
             return HttpResponse("购物车最多添加10件服务")
         search_dict = dict()
-        search_dict["user"]=user
-        search_dict["se_id"]=se_id
+        search_dict["user"] = user
+        search_dict["se_id"] = se_id
         cart = Cart.objects.filter(**search_dict)[0]
         if not cart:
             CartAdd = Cart()
@@ -49,11 +50,24 @@ class ServiceView(View):
                 cart.save()
                 return HttpResponse("购物车该服务时间修改成功")
 
+
 class ShopView(View):
     def get(self, request):
         username, services_sort, is_login = Util.get_basic_info(request)
-        return render(request, "shop.html",
-                      {"username": username, "services_sort": services_sort, "is_login": is_login})
+        data = request.GET
+        s_id = int(data.get("s_id"))
+        page_num = int(data.get("page"))
+        shop = Shop.objects.get(id=s_id)
+        services = Service.objects.filter(shop_id=s_id)
+        for service in services:
+            
+        response_data = {
+            "username": username,
+            "services_sort": services_sort,
+            "is_login": is_login,
+            "shop": shop,
+        }
+        return render(request, "shop.html", response_data)
 
 
 class PayView(View):
@@ -78,6 +92,3 @@ class PayView(View):
         return render(request, "pay.html",
                       {"services": services, "total_cost": total_cost, "user": user, "username": username,
                        "services_sort": services_sort, "is_login": is_login})
-
-
-
