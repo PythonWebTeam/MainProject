7b26e7cf1d65c7cf9fd4e164bf917549a1223437
hofee <2716863922@qq.com>
2021-07-15 21:41:38 +0800

    Update test.py

--- HomeServiceMall/utils/test.py
+++ HomeServiceMall/utils/test.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from account.models import Service
 
 def pay_test_view(request):
-    return render(request,"pay.html",{"result":"支付成功"})
+    return render(request,"result.html",{"result":"支付成功"})
 def test_view(request):
     services = Service.objects.all()
     for service in services:
